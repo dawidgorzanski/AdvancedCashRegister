4668bf6a0ab02ff09f08f71a3040d39694a605c1
Alicja Zoń <alazon@wp.pl>
2018-04-14 17:20:05 +0200

    Changes in payment window

--- src/main/java/cashregister/controllers/PaymentWindowController.java
+++ src/main/java/cashregister/controllers/PaymentWindowController.java
@@ -3,50 +3,41 @@ package cashregister.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PaymentWindowController implements Initializable {
 
     @FXML
-    private TextField textFieldDisplay;
+    private Label cashLabel, changeLabel, plnLabel1, plnLabel2;
+    @FXML
+    private TextField cashField, changeField;
+    @FXML
+    private Button confirmButton;
 
     @FXML
-    private void handleEnterButtonAction(ActionEvent event) {
-        String value = textFieldDisplay.getText();
-        int number;
-
-        try {
-            number = Integer.parseInt(value);
-
-        } catch (NumberFormatException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Błąd");
-            alert.setHeaderText("Błąd");
-            alert.setContentText("Wpisz poprawną kwotę");
-
-            alert.showAndWait();
-        }
-        textFieldDisplay.clear();
-    }
-
-    @FXML
-    private void handleNumberButtonAction(ActionEvent event) {
-        String text = ((Button) event.getSource()).getText();
-        textFieldDisplay.appendText(text);
-    }
-
-    @FXML
-    private void handleClearButtonAction(ActionEvent event) {
-        textFieldDisplay.clear();
+    private void handleCashButtonAction(ActionEvent event) {
+        cashField.setVisible(true);
+        changeField.setVisible(true);
+        cashLabel.setVisible(true);
+        changeLabel.setVisible(true);
+        plnLabel1.setVisible(true);
+        plnLabel2.setVisible(true);
+        confirmButton.setVisible(true);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        cashField.setVisible(false);
+        changeField.setVisible(false);
+        cashLabel.setVisible(false);
+        changeLabel.setVisible(false);
+        plnLabel1.setVisible(false);
+        plnLabel2.setVisible(false);
+        confirmButton.setVisible(false);
 
     }
 }
