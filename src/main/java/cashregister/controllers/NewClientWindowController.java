9918ece2e7f48ac42cd6c9432c2d6dd2e1da418c
jpalwal <jagoda.palwal@gmail.com>
2018-04-22 15:53:48 +0200

    new client handler added

--- src/main/java/cashregister/controllers/NewClientWindowController.java
+++ src/main/java/cashregister/controllers/NewClientWindowController.java
@@ -1,4 +1,13 @@
 package cashregister.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+
 public class NewClientWindowController {
+
+    @FXML
+    private void handleCancelButtonAction(ActionEvent event) {
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
 }
