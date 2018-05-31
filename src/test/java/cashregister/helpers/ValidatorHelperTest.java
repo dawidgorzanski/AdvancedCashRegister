6c6658db5656b00a4c7e84f19501d78106ba2e62
BartlomiejSzozda <bartekszozda19@gmail.com>
2018-05-31 22:38:45 +0200

    ValidatorHelper and ProductDefinitionModule tested

---
+++ src/test/java/cashregister/helpers/ValidatorHelperTest.java
@@ -0,0 +1,195 @@
+package cashregister.helpers;
+
+import cashregister.model.User;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidatorHelperTest {
+    //@Before
+    //private ValidatorHelper validatorHelper = new ValidatorHelper();
+    @Test
+    public void validatePhoneNumber_NumberNotNumeric_ReturnFalse() {
+        assertFalse(ValidatorHelper.validatePhoneNumber("887228a78"));
+    }
+
+    @Test
+    public void validatePhoneNumber_NumberLengthNot9Not0_ReturnFalse() {
+        assertFalse(ValidatorHelper.validatePhoneNumber("12389238"));
+    }
+
+    @Test
+    public void validatePhoneNumber_NumberLengthIs0_ReturnTrue() {
+        assertTrue(ValidatorHelper.validatePhoneNumber(""));
+    }
+
+    @Test
+    public void validatePhoneNumber_NumberIsNumericLengthIs9_ReturnTrue() {
+        assertTrue(ValidatorHelper.validatePhoneNumber("849324221"));
+    }
+
+    @Test
+    public void validateClientBarcode_BarcodeNotNumeric_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateClientBarcode("887228887228a78"));
+    }
+
+    @Test
+    public void validateClientBarcode_NumberLengthNot15_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateClientBarcode("13238392383421"));
+    }
+
+    @Test
+    public void validateClientBarcode_NumberIsNumericNumberLengthIs15_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateClientBarcode("132383923834281"));
+    }
+
+    @Test
+    public void validateNameNotEmpty_NameEmpty_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateClientBarcode(""));
+    }
+
+    @Test
+    public void validateNameNotEmpty_NameNotEmpty_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateNameNotEmpty("Name"));
+    }
+
+    @Test
+    public void validateMailAddress_EmailEmpty_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateMailAddress(""));
+    }
+
+    @Test
+    public void validateMailAddress_EmailWithoutSymbolAt_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateMailAddress("asdf.com"));
+    }
+
+    @Test
+    public void validateMailAddress_EmailWithSymbolAt_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateMailAddress("asdf@sss.com"));
+    }
+
+    @Test
+    public void validateProductBarcodeLength_BarcodeNotNumeric_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateProductBarcodeLength("8872288228a78"));
+    }
+
+    @Test
+    public void validateProductBarcodeLength_NumberLengthNot13_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateProductBarcodeLength("132383383421"));
+    }
+
+    @Test
+    public void validateProductBarcodeLength_NumberIsNumericNumberLengthIs13_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateProductBarcodeLength("1323839238342"));
+    }
+
+    @Test
+    public void validateProductBarcodeLength_NameEmpty_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateProductBarcodeLength(""));
+    }
+
+    @Test
+    public void validateQuantity_QuantityWithComa_ReturnFalse() {
+        assertFalse( ValidatorHelper.validateQuantity("4934,2"));
+    }
+
+    @Test
+    public void validateQuantity_QuantityWithDot_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateQuantity("43.23"));
+    }
+
+    @Test
+    public void validateQuantity_QuantityIsInt_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateQuantity("4564"));
+    }
+
+    @Test
+    public void validateQuantity_QuantityIsntNumber_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateQuantity("asd"));
+    }
+
+    @Test
+    public void validateQuantity_QuantityIsPartlyNumber_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateQuantity("342.2ds"));
+    }
+
+    @Test
+    public void validatePrice_PriceWithComa_ReturnFalse() {
+        assertFalse( ValidatorHelper.validatePrice("1234,2"));
+    }
+
+    @Test
+    public void validatePrice_PriceWithDot_ReturnTrue() {
+        assertTrue(ValidatorHelper.validatePrice("6666.5"));
+    }
+
+    @Test
+    public void validatePrice_PriceIsInt_ReturnTrue() {
+        assertTrue(ValidatorHelper.validatePrice("5"));
+    }
+
+    @Test
+    public void validatePrice_PriceIsntNumber_ReturnFalse() {
+        assertFalse(ValidatorHelper.validatePrice("asdas"));
+    }
+
+    @Test
+    public void validatePrice_PriceIsPartlyNumber_ReturnFalse() {
+        assertFalse(ValidatorHelper.validatePrice("3342.2ds"));
+    }
+
+    @Test
+    public void validatePrice_PriceIsLowerThan0_ReturnFalse() {
+        assertFalse(ValidatorHelper.validatePrice("-20"));
+    }
+
+    @Test
+    public void validateIsNotNull_ObjectIsNull_ReturnTrue() {
+        assertFalse(ValidatorHelper.validateIsNotNull(null));
+    }
+
+    @Test
+    public void validateIsNotNull_ObjectIsntNull_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateIsNotNull(new Object()));
+    }
+
+    @Test
+    public void validateName_editIsFalseUserIsNull_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateName("name", null, false, "any"));
+    }
+
+    @Test
+    public void validateName_editIsTrueNameEqualsOldName_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateName("sameName", null, true, "sameName"  ));
+    }
+
+    @Test
+    public void validateName_editIsFalseUserNotNull_ReturnFalse() {
+        User user = new User();
+        assertFalse(ValidatorHelper.validateName("sameName", user, false, "sameName"  ));
+    }
+
+    @Test
+    public void validateInput_PasswordShorterThan4_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateInput("123", "realName", new Object()  ));
+    }
+
+    @Test
+    public void validateInput_NameIsEmpty_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateInput("12345678", "", new Object()  ));
+    }
+
+    @Test
+    public void validateInput_PersonIsntAdmin_ReturnFalse() {
+        assertFalse(ValidatorHelper.validateInput("12345678", "realName", null  ));
+    }
+
+    @Test
+    public void validateInput_PasswordLongerThan3NameNotEmptyPersonIsAdmin_ReturnTrue() {
+        assertTrue(ValidatorHelper.validateInput("12345678", "realName", new Object()  ));
+    }
+
+
+
+
+}
---
+++ src/test/java/cashregister/modules/ProductDefinitionModuleTest.java
@@ -0,0 +1,162 @@
+package cashregister.modules;
+
+import cashregister.dao.interfaces.IProductDefinitionDao;
+import cashregister.model.ProductDefinition;
+import cashregister.modules.interfaces.IProductDefinitionModule;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProductDefinitionModuleTest {
+    @Mock
+    private ProductDefinition productDefinition1, productDefinition2, productDefinition3;
+
+    @Mock
+    private IProductDefinitionDao productDefinitionDaoMock;
+
+    @InjectMocks
+    private IProductDefinitionModule productDefinitionModule = new ProductDefinitionModule(productDefinitionDaoMock);
+
+    @Test
+    public void getAllProductsMethodReturnsAllProductsFromDatabaseTest(){
+
+        //Arrange
+        List<ProductDefinition> productsList = new ArrayList<ProductDefinition>();
+        productsList.add(productDefinition1);
+        productsList.add(productDefinition2);
+        productsList.add(productDefinition3);
+        Mockito.when(productDefinitionDaoMock.getAll()).thenReturn(productsList);
+
+        //Act
+        List<ProductDefinition> productsTest = productDefinitionModule.getAllProducts();
+
+        //Assert
+        assertEquals(productsTest, productsList);
+    }
+
+    @Test
+    public void addProductMethodAddsProductToDatabaseTest(){
+        //Arrange
+        List<ProductDefinition> ProductsList = new ArrayList<ProductDefinition>();
+
+        Mockito.doAnswer( (Answer) invocation -> {
+            ProductDefinition arg0 = invocation.getArgumentAt(0,ProductDefinition.class);
+            ProductsList.add(arg0);
+            return null;
+        }).when(productDefinitionDaoMock).saveOrUpdate(Matchers.anyObject());
+
+        //Act
+        productDefinitionModule.addProduct(productDefinition1);
+        productDefinitionModule.addProduct(productDefinition2);
+
+        //Assert
+        assertTrue(ProductsList.size() == 2  );
+    }
+
+    @Test
+    public void updateProductMethodUpdateProductsInDatabaseTest()
+    {
+        //Arrange
+        ArrayList<ProductDefinition> ProductDefinitionList = new ArrayList<ProductDefinition>();
+        ProductDefinitionList.add(productDefinition1);
+
+        ProductDefinition productForUpdate = new ProductDefinition(4, "Chleb", 100.0, 2.5, "1234123412341", true, false);
+        ProductDefinitionList.add(productForUpdate);
+
+
+        Mockito.doAnswer( (Answer) invocation -> {
+            ProductDefinition arg0 = invocation.getArgumentAt(0,ProductDefinition.class);
+            ProductDefinitionList.set(ProductDefinitionList.indexOf(arg0), arg0);
+            return null;
+        }).when(productDefinitionDaoMock).update(Matchers.anyObject());
+
+
+        //Act
+        productForUpdate.setName("Maslo");
+        productDefinitionModule.updateProduct(productForUpdate);
+
+
+        //Assert
+        assertTrue(ProductDefinitionList.contains(productForUpdate));
+        assertEquals("Maslo", ProductDefinitionList.get(ProductDefinitionList.indexOf(productForUpdate)).getName() );
+    }
+
+    @Test
+    public void deleteProductMethodDeletesProductFromDatabaseTest(){
+        //Arrange
+        List<ProductDefinition> productsList = new ArrayList<ProductDefinition>();
+        productsList.add(productDefinition1);
+        productsList.add(productDefinition2);
+        productsList.add(productDefinition3);
+
+        Mockito.doAnswer( (Answer)invocation-> {
+            productsList.remove(invocation.getArgumentAt(0,ProductDefinition.class));
+            return null;
+        }).when(productDefinitionDaoMock).delete(Matchers.anyObject());
+
+        //Act
+        productDefinitionModule.deleteProduct(productDefinition2);
+        //Assert
+        assertTrue(productsList.size() == 2);
+        productDefinitionModule.deleteProduct(productDefinition1);
+        assertTrue(productsList.size() == 1);
+    }
+
+    @Test
+    public void getProductByBarcodeReturnsCorrectProductTest(){
+        //Arrange
+        String barcode = "1234123412341";
+        Mockito.when(productDefinitionDaoMock.getByBarcode(barcode)).thenReturn(productDefinition1);
+
+        //Act
+        ProductDefinition productTest = productDefinitionModule.getByBarcode(barcode);
+
+        //Assert
+        assertTrue(productTest.equals(productDefinition1));
+    }
+
+    @Test
+    public void getProductByNameReturnsCorrectProductTest(){
+        //Arrange
+        String name = "name";
+
+        List<ProductDefinition> productsList = new ArrayList<>();
+        productsList.add(productDefinition1);
+        ObservableList<ProductDefinition> productsObservableList = FXCollections.observableArrayList(productsList);
+
+        Mockito.when(productDefinitionDaoMock.getByName(name)).thenReturn(productsObservableList);
+
+        //Act
+        List<ProductDefinition> productsListTest = productDefinitionModule.getByName(name);
+
+        //Assert
+        assertTrue(productsListTest == productsObservableList);
+    }
+
+
+
+}
+
+
+
+
+
+
+
