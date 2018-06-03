7fafd298999613161d966dc47f3533c93a1d19a8
Jan <jan.wrona@gmail.com>
2018-06-03 22:47:11 +0200

    ProductListModule tested.

--- src/test/java/cashregister/modules/ProductListModuleTest.java
+++ src/test/java/cashregister/modules/ProductListModuleTest.java
@@ -4,6 +4,7 @@ import cashregister.dao.interfaces.IProductDefinitionDao;
 import cashregister.model.ProductDefinition;
 import cashregister.model.ProductForSale;
 import cashregister.modules.interfaces.IProductsListModule;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -24,46 +25,78 @@ public class ProductListModuleTest {
     }
 
     @Test
-    public void addProductMethodIsShoppingListUpdatedCorrectlyTest(){
-        final String barcode1 = "1234567891234";
-        final String barcode2 = "0123456789123";
+    public void addProductMethodIsShoppingListUpdatedCorrectlyWhenAddingProductsWithDifferentProductDefinitionTest(){
+        final String barcode1 = "barcode1";
+        final String barcode2 = "barcode2";
         ProductDefinition productDefinition1 = new ProductDefinition();
         productDefinition1.setPrice(110.4);
         productDefinition1.setName("product1");
+        productDefinition1.setId(1);
         ProductDefinition productDefinition2 = new ProductDefinition();
         productDefinition2.setPrice(30);
         productDefinition2.setName("product2");
+        productDefinition2.setId(2);
         Mockito.when(productDefinitionDao.getByBarcode(barcode1)).thenReturn(productDefinition1);
         Mockito.when(productDefinitionDao.getByBarcode(barcode2)).thenReturn(productDefinition2);
 
         ProductsListModule test = new ProductsListModule(productDefinitionDao);
 
-        System.out.println(test.getShoppingList().size());
+        Assert.assertTrue(test.getShoppingList().size() == 0);
         test.addProduct(barcode1);
-        System.out.println(test.getShoppingList().size());
+        Assert.assertTrue(test.getShoppingList().size() == 1);
         test.addProduct(barcode2);
-        System.out.println(test.getShoppingList().size());
+        Assert.assertTrue(test.getShoppingList().size() == 2);
+        Assert.assertTrue(test.getShoppingList().get(0).getPrice() == 110.4);
+        Assert.assertTrue(test.getShoppingList().get(1).getName().equals("product2"));
+    }
 
-        for(ProductForSale it : test.getShoppingList()){
-            System.out.println(it.getName());
-            System.out.println(it.getPrice());
-            System.out.println(it.getQuantity());
-        }
+    @Test
+    public void addProductMethodIsShoppingListUpdatedCorrectlyWhenAddingProductsWithTheSameIdTest(){
+        final String barcode1 = "barcode1";
+        final String barcode2 = "barcode2";
+        ProductDefinition productDefinition1 = new ProductDefinition();
+        productDefinition1.setPrice(110.4);
+        productDefinition1.setName("product1");
+        productDefinition1.setId(1);
+        ProductDefinition productDefinition2 = new ProductDefinition();
+        productDefinition2.setPrice(30);
+        productDefinition2.setName("product2");
+        productDefinition2.setId(1);
+        Mockito.when(productDefinitionDao.getByBarcode(barcode1)).thenReturn(productDefinition1);
+        Mockito.when(productDefinitionDao.getByBarcode(barcode2)).thenReturn(productDefinition2);
 
+        ProductsListModule test = new ProductsListModule(productDefinitionDao);
+
+        Assert.assertTrue(test.getShoppingList().size() == 0);
+        test.addProduct(barcode1);
+        Assert.assertTrue(test.getShoppingList().size() == 1);
+        Assert.assertTrue(test.getShoppingList().get(0).getQuantity() == 1.0);
+        test.addProduct(barcode2);
+        Assert.assertTrue(test.getShoppingList().size() == 1);
+        Assert.assertTrue(test.getShoppingList().get(0).getQuantity() == 2.0);
     }
 
     @Test
     public void getTotalPriceMethodIsTotalPriceBeingCalculatedCorrectly(){
         final String barcode1 = "barcode1";
         final String barcode2 = "barcode2";
+        final String barcode3 = "barcode3";
         ProductDefinition productDefinition1 = new ProductDefinition();
         productDefinition1.setPrice(110.4);
         productDefinition1.setName("product1");
+        productDefinition1.setId(1);
+        productDefinition1.setCountable(true);
         ProductDefinition productDefinition2 = new ProductDefinition();
         productDefinition2.setPrice(30);
         productDefinition2.setName("product2");
+        productDefinition2.setId(2);
+        ProductDefinition productDefinition3 = new ProductDefinition();
+        productDefinition3.setId(3);
+        productDefinition3.setName("product3");
+        productDefinition3.setPrice(123.3);
         Mockito.when(productDefinitionDao.getByBarcode(barcode1)).thenReturn(productDefinition1);
         Mockito.when(productDefinitionDao.getByBarcode(barcode2)).thenReturn(productDefinition2);
+        Mockito.when(productDefinitionDao.getByBarcode(barcode3)).thenReturn(productDefinition3);
 
         ProductsListModule test = new ProductsListModule(productDefinitionDao);
 
@@ -71,48 +104,12 @@ public class ProductListModuleTest {
         test.addProduct(barcode2);
 
         double returnedPrice = test.getTotalPrice();
+        Assert.assertTrue(returnedPrice == (productDefinition1.getPrice() + productDefinition2.getPrice()));
 
-        System.out.println(returnedPrice);
+        test.addProduct(barcode3);
+        returnedPrice = test.getTotalPrice();
+        Assert.assertTrue(returnedPrice == (productDefinition1.getPrice() + productDefinition2.getPrice() + productDefinition3.getPrice()));
     }
-
 
 
 }
-
-
-/*
-    public void addProduct(String barcode)
-    {
-        ProductDefinition productDefinition = productDefinitionDao.getByBarcode(barcode);
-        if(productDefinition != null)
-        {
-            if (shoppingListContainsProduct(productDefinition)) {
-                ProductForSale productForSale = findProductByProductDefinition(productDefinition);
-                productForSale.setQuantity(productForSale.getQuantity() + 1);
-            }
-            else {
-                ProductForSale newItem = new ProductForSale(productDefinition);
-                shoppingList.add(newItem);
-            }
-        }
-        else
-        {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Błąd");
-            alert.setHeaderText("Błąd");
-            alert.setContentText("Podany kod nie znajduje się w bazie");
-            alert.showAndWait();
-        }
-
-    public double getTotalPrice() {
-        if (this.shoppingList == null || this.shoppingList.size() == 0)
-            return 0;
-
-        double result = 0;
-        for(ProductForSale productForSale : this.shoppingList) {
-            result += productForSale.getTotalPrice();
-        }
-
-        return result;
-    }
-    */
